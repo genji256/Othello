20e20a5b064bc6ca10e7edf4f80d0e4f43bbf0d1
genji256 <sosesusisa@gmail.com>
2013-06-22 22:32:48 +0900

    コマを置けるようにした

--- Othello.rb
+++ Othello.rb
@@ -15,6 +15,9 @@ $boardArray = [[], [], [], [], [], [], [], []]
 #順番：1なら黒-1なら白の番
 $turn = 1
 
+#1マスの中心座標
+$centerPoint = []
+
 TkRoot.new do
 	title("オセロ")
 end
@@ -57,6 +60,7 @@ $c = TkCanvas.new(f1) do
 		proc do |x, y| 
 			lx.text(x.to_s)
 			ly.text(y.to_s)
+			setChessman(x , y)
 		end,
 		"%x %y"
 	)
@@ -66,7 +70,9 @@ end
 def init
 	i = 0
 	x = 17
+
 	while 8 > i
+		$centerPoint[i] = x
 		j = 0
 		y = 17
 		while 8 > j
@@ -92,6 +98,70 @@ def init
 		x += 32
 		i += 1
 	end
+end
+
+#座標から配列のインデックスを割り出す
+def convertPoint(p)
+	if 33 > p
+		i = 0
+	elsif 65 > p
+		i = 1
+	elsif 97 > p
+		i = 2
+	elsif 129 > p
+		i = 3
+	elsif 161 > p
+		i = 4
+	elsif 193 > p
+		i = 5
+	elsif 225 > p
+		i = 6
+	else 
+		i = 7
+	end
+
+	return i
+end
+
+#座標から各マスの中心座標を割り出す
+def getCenterPoint(p)
+	if 33 > p
+		i = $centerPoint[0]
+	elsif 65 > p
+		i = $centerPoint[1]
+	elsif 97 > p
+		i = $centerPoint[2]
+	elsif 129 > p
+		i = $centerPoint[3]
+	elsif 161 > p
+		i = $centerPoint[4]
+	elsif 193 > p
+		i = $centerPoint[5]
+	elsif 225 > p
+		i = $centerPoint[6]
+	else 
+		i = $centerPoint[7]
+	end
+
+	return i
+end
+
+def setChessman(x, y)
+	cX = getCenterPoint(x)
+	cY = getCenterPoint(y)
+	#Tk.messageBox("message" => "#{cX},#{cY}")
+	if 1 == $turn
+		TkcImage.new($c, cX, cY) do
+			image $blackImg
+		end
+		$turn = -1
+	else 
+		TkcImage.new($c, cX, cY) do
+			image $whiteImg
+		end
+		$turn = 1
+	end
+
 end
 
 init
